8a98245860d6532aa4b933beeaace4e99ef72fd1
Eliamd-c <elamd.se@gmail.com>
2026-08-31 22:05:59 -0500

    Integrar Google Tag Manager (GTM-NQW5S9WG)

--- Pagina Web Distribuidores/distribuidores-app/src/app/layout.tsx
+++ Pagina Web Distribuidores/distribuidores-app/src/app/layout.tsx
@@ -36,6 +36,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} ${greatVibes.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQW5S9WG"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NQW5S9WG');
+          `}
+        </Script>
+
+        {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -53,6 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <noscript>
           <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=4218954695068961&ev=PageView&noscript=1" alt="" />
         </noscript>
+
         {children}
       </body>
     </html>
